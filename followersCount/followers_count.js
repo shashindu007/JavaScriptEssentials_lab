303858f187f0ecf3c3e5c49e055bc569d8dc8cf5
shashindu007 <shashindukalshan173@gmail.com>
2026-05-05 04:17:26 -0400

    followers_count lab-practice tasks

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -15,5 +15,14 @@ function checkCountValue() {
       alert("Your Instagram post gained 10 followers! Congratulations!");
     } else if (count === 20) {
       alert("Your Instagram post gained 20 followers! Keep it up!");
+    }else if(count===0){
+        alert(" Followers count has been reset");
     }
+}
+
+function resetCount(){
+    count = 0;
+    displayCount(); 
+    checkCountValue();
+
 }
