5be9c121c6143068a6e8e1e2d45ce509855bd05c
shashindu007 <shashindukalshan173@gmail.com>
2026-05-08 03:37:12 -0400

    restaurantMenu lab Practice task

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,8 +1,8 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak - $10', 'Pasta -$14', 'Burger-$17', 'Salmon-$18'];
+const dessertMenu = ['Cake-$3', 'Ice Cream -$5', 'Pudding -$4', 'Fruit Salad-$5'];
 
- const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+ const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join("");
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
   
         let mainCourseItem = '';
